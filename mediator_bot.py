337f468d036f1dc815e85706f62230adfc79c7e0
themediators <34284428+themediators@users.noreply.github.com>
2017-12-07 12:05:03 -0500

    Update mediator_bot.py

--- mediator_bot.py
+++ mediator_bot.py
@@ -1,55 +1,64 @@
-import praw
-import xlrd
-reddit = praw.Reddit(client_id = 'nk7_2ZlG2QSpmw',
+import praw   #library for reddit comment pulling
+import xlrd   #library for reading excel sheets
+reddit = praw.Reddit(client_id = 'nk7_2ZlG2QSpmw',                                     #Connects the bot to a sub-reddit
                     client_secret = 'hAi0KInm8DnchFOU2MT1vLHt6kI',
                     password = 'principle05',
                     user_agent = 'Python/mediatorapp:v1 (request by u/TheMediators4984)',
                     username = 'TheMediators4984')
 print(reddit.user.me())
 
-d = {}
+bad = {}          #Three dictionaries are initialized for personal identifiers, negations, and negative words
+ident = {}
+neg = {}
 wb = xlrd.open_workbook('Dictionary.xls')
 sh = wb.sheet_by_index(0)
-for i in range(3):
+for i in range(25):                       #The dictionaries are populated by the excel spreadsheet
     cell_value_word = sh.cell(i,0).value
     cell_value_numb = sh.cell(i,1).value
-    d[cell_value_word] = cell_value_numb
+    ident[cell_value_word] = cell_value_numb
+for i in range (26, 50):
+    cell_value_word = sh.cell(i,0).value
+    cell_value_numb = sh.cell(i,1).value
+    neg[cell_value_word] = cell_value_numb
+for i in range (51, 100):
+    cell_value_word = sh.cell(i,0).value
+    cell_value_numb = sh.cell(i,1).value
+    bad[cell_value_word] = cell_value_numb
 
-for comment in reddit.subreddit('pythonforengineers').stream.comments(): #dynamic
+for comment in reddit.subreddit('pythonforengineers').stream.comments(): #So long as this bot runs, dynamically receive new comments
     if(comment.link_id == "t3_7cy2wk" and comment.saved == False):
         comment.save(category=None)
         
-        totalSentiment = 0
+        totalSentiment = 0         #Initialize total comment sentiment
         
-        thisComment = comment.body
+        thisComment = comment.body      #Get the comment as a string
         print("Full comment: " + thisComment)
         
-        sub_sentences = thisComment.split(",")
+        sub_sentences = thisComment.split(",")  #Split it into components
         
-        for i in range (0, len(sub_sentences)):           #for each sub-sentence
-            mult_ident = False        #reset multipliers
+        for i in range (0, len(sub_sentences)):           #for each component...
+            mult_ident = False        #Initialize identification and negation multiplier
             mult_neg = False
             
-            sentiment = 0
-            words = sub_sentences[i].split(" ")
+            sentiment = 0             #Initialize component sentiment
+            words = sub_sentences[i].split(" ")   #Split component into words
             
-            for j in range (0, len(words)):               #for each word in sub-sentence
+            for j in range (0, len(words)):               #for each word in component
                 currentWord = words[j].lower()
                 
-                if currentWord in d:
-                    if (currentWord == "you"):            #check for identifiers
-                        mult_ident = True
-                    elif (currentWord == "not"):          #check for negators 
-                        mult_neg = True
-                    else:
-                        sentiment = sentiment + d[currentWord];
+                if currentWord in bad:                    #If in negative word dictionary...
+                    sentiment = sentiment + d[currentWord];  #...add to sentiment
+                elif currentWord in neg:                  #If in negation or identifer dictionary...
+                    mult_neg = True                       #...save multiplier for later
+                elif currentWord in ident:
+                    mult_ident = True
                         
-            if (mult_ident == True):                      #apply multipliers
-                sentiment = sentiment * d['you']
+            if (mult_ident == True):                      #Apply multipliers if found
+                sentiment = sentiment * 1.5
             if (mult_neg == True):
-                sentiment = sentiment * d['not']
+                sentiment = sentiment * -1
             
-            if (sentiment > 0):                           #add to total if negative
+            if (sentiment > 0):                           #Add component sentiment to total if negative
                 totalSentiment = totalSentiment + sentiment
                         
         print("Sentiment: %f" %totalSentiment)
